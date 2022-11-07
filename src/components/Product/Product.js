7ee86b8dc6d530e8ce65aeb1a675c894354d0546
asifhossain2k20 <asif.hossain2k20@gmail.com>
2022-11-07 23:13:03 +0600

    Cart ready

---
+++ src/components/Product/Product.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Product.css'
+const Product = (props) => {
+    const {name,img,seller,price,ratings}=props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price : ${price}</p>
+                <p><small>Seller : {seller}</small></p>
+                <p><small>Ratings: {ratings}</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>ADD TO CART</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
